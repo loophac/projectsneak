f23584e549394c3a0e9aedbbb1a64e09b335df5f
Christian Doucette <dutytocountry@gmail.com>
2018-01-16 13:55:12 -0500

    Add files via upload
    Added database

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 import socket               # Import socket module
 import time 
+
+username = "Loophac"
+type = "Rock"
+
 while True:
     s = socket.socket()         # Create a socket object
     s.connect(('localhost', 12345))
-    s.sendall("-420,600")
-    s.close()    
+    #s.send("-420,600")
+    msg1 = "-420,600"
+    s.sendall(msg1.encode())
+    s.recv(2048)  # but we don't really care what the message is
+    msg2 = username
+    s.sendall(msg2.encode()) 
+    s.recv(2048)  # but we don't really care what the message is
+    msg2 = type
+    s.sendall(msg2.encode())   
     time.sleep(6)   
 
--- server.py
+++ server.py
@@ -1,11 +1,42 @@
 import socket               # Import socket module
-
+import sqlite3
+import struct
 s = socket.socket()         # Create a socket object
 s.bind(('127.0.0.1', 12345))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
+
+def db():
+   conn = sqlite3.connect('db/globaldata.db')
+   db = conn
+   cursor = db.cursor()
+   coordinate = msg1
+   username = msg2
+   taste = msg3
+ 
+   cursor.execute('''INSERT INTO coordinates(Coordinates, Username, Taste)
+                  VALUES(?,?,?)''', (coordinate,username,taste))
+ 
+   print('Data Entered')
+ 
+   db.commit()
+   db.close()
+   return 
+
+
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print 'Got connection from', addr
-   print c.recv(1024)
+   msg1 = c.recv(128).decode()
+   c.send(b'Got it')
+   msg2 = c.recv(128).decode()
+   c.send(b'Got it')
+   msg3 = c.recv(128).decode()
+   #data = c.recv(1024)
+   #print data
+   #stringdata = data.decode('utf-8')
    c.close()                # Close the connection
+   db()
+ 
+
+

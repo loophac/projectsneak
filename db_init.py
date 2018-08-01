61b97b023a457e465541698c4cc1699a96fa9841
Christian Doucette <dutytocountry@gmail.com>
2018-08-01 12:39:36 -0400

    Improved DB

--- db_init.py
+++ db_init.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 sqlite_file = 'globaldata.db'    # name of the sqlite database file
-table_name1 = 'coordinates'  # name of the table to be created
-table_name2 = 'username'  # name of the table to be created
-table_name3 = 'taste'
-new_field = 'my_1st_column' # name of the column
-field_type = 'INTEGER'  # column data type
+table_name = 'userdata'  # name of the table to be created
+new_field = 'userid' # name of the column
+field_type = 'INTEGER'
+new_field = 'geolocation'
+field_type = 'INTEGER'
+new_field = 'fav'
+field_type = 'TEXT'  # column data type
 
 # Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
@@ -13,14 +15,9 @@ c = conn.cursor()
 
 # Creating a new SQLite table with 1 column
 c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-        .format(tn=table_name1, nf=new_field, ft=field_type))
+        .format(tn=table_name, nf=new_field, ft=field_type))
 
-# Creating a second table with 1 column and set it as PRIMARY KEY
-# note that PRIMARY KEY column must consist of unique values!
-c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn=table_name2, nf=new_field, ft=field_type))
 
 # Committing changes and closing the connection to the database file
 conn.commit()
 conn.close()
-
